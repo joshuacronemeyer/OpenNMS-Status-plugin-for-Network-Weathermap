ec1b9f16913e4a346b694cf24f022c75cf0baad1
Josh Cronemeyer <joshuacronemeyer+github@gmail.com>
2011-12-11 14:57:07 -0800

    really make tabs spaces

--- WeatherMapDataSource_onms.php
+++ WeatherMapDataSource_onms.php
@@ -6,27 +6,27 @@
 
 class WeatherMapDataSource_onms extends WeatherMapDataSource {
 
-	function Init(&$map)
-	{
-		return(TRUE);
-	}
+  function Init(&$map)
+  {
+    return(TRUE);
+  }
 
 
-	function Recognise($targetstring)
-	{
-		return preg_match("/^onms:(\d+)$/",$targetstring,$matches);
-	}
-	
-	function IsPrimaryInterface($ip_interface)
-	{
-		$is_primary = $ip_interface->getAttribute("snmpPrimary");
-		return ($is_primary == "P");
-	}
-	
-	function OpenNMSInterfacesUrl($node_id, $map)
-	{
-	
-		$onmshttps = $map->get_hint('opennms_https');
+  function Recognise($targetstring)
+  {
+    return preg_match("/^onms:(\d+)$/",$targetstring,$matches);
+  }
+  
+  function IsPrimaryInterface($ip_interface)
+  {
+    $is_primary = $ip_interface->getAttribute("snmpPrimary");
+    return ($is_primary == "P");
+  }
+  
+  function OpenNMSInterfacesUrl($node_id, $map)
+  {
+  
+    $onmshttps = $map->get_hint('opennms_https');
     $onmshost = $map->get_hint('opennms_host');
     $onmsport = intval($map->get_hint('opennms_port'));
     $onmsbaseurl = $map->get_hint('opennms_base_url');
@@ -38,62 +38,62 @@ class WeatherMapDataSource_onms extends WeatherMapDataSource {
     if($onmsbaseurl == '') $onmsbaseurl = "/opennms";
 
     return "${onmsproto}://${onmsuser}:${onmspass}@${onmshost}:${onmsport}${onmsbaseurl}/rest/nodes/$node_id/ipinterfaces";
-	}
+  }
 
-	function GetInterfaceList($node_id, $map)
-	{
-		$fetchurl = $this->OpenNMSInterfacesUrl($node_id, $map);
-		$fd = fopen($fetchurl, "rb");
+  function GetInterfaceList($node_id, $map)
+  {
+    $fetchurl = $this->OpenNMSInterfacesUrl($node_id, $map);
+    $fd = fopen($fetchurl, "rb");
 
-		if (! $fd) {
-			debug("OpenNMS RrdSummary ReadData: Couldn't open ($fetchurl)");
-			return( array('NULL', 'NULL', $data_time));
-		}
+    if (! $fd) {
+      debug("OpenNMS RrdSummary ReadData: Couldn't open ($fetchurl)");
+      return( array('NULL', 'NULL', $data_time));
+    }
 
-		$domdoc = new DOMDocument();
-		$domdoc->loadXML(stream_get_contents($fd));
-		fclose($fd);
-		if (! $domdoc) {
-			debug("OpenNMS RrdSummary ReadData: Failed to parse XML from ($fetchurl). Returning all NULL with timestamp $data_time. \n");
-			return( array('NULL', 'NULL', $data_time));
-		}
+    $domdoc = new DOMDocument();
+    $domdoc->loadXML(stream_get_contents($fd));
+    fclose($fd);
+    if (! $domdoc) {
+      debug("OpenNMS RrdSummary ReadData: Failed to parse XML from ($fetchurl). Returning all NULL with timestamp $data_time. \n");
+      return( array('NULL', 'NULL', $data_time));
+    }
 
-		return $domdoc->getElementsByTagName("ipInterface");
+    return $domdoc->getElementsByTagName("ipInterface");
 
-	}
-	
-	function IsDown($ip_interface)
-	{
-		$is_down = $ip_interface->getAttribute("isDown");
-		if ($is_down == "true"){
-			return 0;
-		} else {
-			return 1;
-		}
-	}
+  }
+  
+  function IsDown($ip_interface)
+  {
+    $is_down = $ip_interface->getAttribute("isDown");
+    if ($is_down == "true"){
+      return 0;
+    } else {
+      return 1;
+    }
+  }
 
-	function ReadData($targetstring, &$map, &$item)
-	{
-		$data[IN] = null;
-		$data[OUT] = null;
-		$data_time = 1;
+  function ReadData($targetstring, &$map, &$item)
+  {
+    $data[IN] = null;
+    $data[OUT] = null;
+    $data_time = 1;
 
-		if(preg_match("/^onms:(\d+)$/",$targetstring,$matches)) {
-			$node_id = $matches[1];
-		}
-		
-		$ip_interfaces = $this->GetInterfaceList($node_id, $map);
-		foreach ($ip_interfaces as $ip_interface) {
-			if ($this->IsPrimaryInterface($ip_interface)){
-				$status = $this->IsDown($ip_interface);
-				$item->add_note("node_status", $status);
-				$data[IN] = $status;
-				$data[OUT] = $status;
-			}
-		}
+    if(preg_match("/^onms:(\d+)$/",$targetstring,$matches)) {
+      $node_id = $matches[1];
+    }
+    
+    $ip_interfaces = $this->GetInterfaceList($node_id, $map);
+    foreach ($ip_interfaces as $ip_interface) {
+      if ($this->IsPrimaryInterface($ip_interface)){
+        $status = $this->IsDown($ip_interface);
+        $item->add_note("node_status", $status);
+        $data[IN] = $status;
+        $data[OUT] = $status;
+      }
+    }
 
-		return( array($data[IN], $data[OUT], $data_time) );
-	}
+    return( array($data[IN], $data[OUT], $data_time) );
+  }
 }
 
 // vim:ts=4:sw=4:
